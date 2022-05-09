8af822f882e4df222d12cfae993cd38f981e8cbb
xaveedra <57705529+xaveedra@users.noreply.github.com>
2022-05-09 12:58:07 +0300

    Add files via upload

--- Personality_test.py
+++ Personality_test.py
@@ -5,28 +5,28 @@ personlity_test = Flask(__name__)
 
 progress_bar = [('progress', 50)]
 
-@personlity_test.route("/")
-def homepage():
-    return render_template("homepage.html",
-                            pagetitle="Homepage",
-                            custom_css="homepage",
-                            page_head="Homepage",
-                            description="This is the Homepage")
+# @personlity_test.route("/")
+# def homepage():
+#     return render_template("homepage.html",
+#                             pagetitle="Homepage",
+#                             custom_css="homepage",
+#                             page_head="Homepage",
+#                             description="This is the Homepage")
 
-@personlity_test.route("/free-personality-test")
+@personlity_test.route("/") # free-personality-test
 def personality_test():
     return render_template("testpage.html",
-                            pagetitle="Testpage",
+                            pagetitle="Myers-Briggs Personality Test",
                             custom_css="testpage",
-                            page_head="Test Page",
+                            page_head="Personality Test",
                             description="This is the Test Page",
                             progress=progress_bar,)
 
-@personlity_test.route("/personality-types")
+@personlity_test.route("/Test-results")
 def types():
-    return render_template("personality-types.html",
-                            pagetitle="Personality-Types",
-                            custom_css="types",
+    return render_template("results.html",
+                            pagetitle="Results Page",
+                            custom_css="results",
                             page_head="Personality Types",
                             description="This is the Presonality Types Page")
 
